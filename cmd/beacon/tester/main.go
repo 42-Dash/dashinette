cd399a2252d91e8ef325aaab5c94859fa8ff74a4
Vitalik <frantsv2004@gmail.com>
2025-01-20 12:01:33 +0100

    chore: split testers

--- cmd/beacon/tester/main.go
+++ cmd/beacon/tester/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-	"dashinette/internals/grader"
-	"dashinette/pkg/parser"
+	"dashinette/internals/grader-beacon"
 	"log"
 	"os"
 )
 
+// {"teamname":"The-Avengers2","repo":"repo/The-Avengers2","league":"open"}
+
 func main() {
 	if len(os.Args) == 2 {
-		config, err := parser.DeserializeTesterConfig([]byte(os.Args[1]))
+		config, err := grader.DeserializeConfig([]byte(os.Args[1]))
 		if err != nil {
 			log.Fatalf("Error: %v", err)
 		}
-		grader.MultistageGraderWithTraces(config)
+		if err = grader.BeaconMultistageGrader(config); err != nil {
+			log.Fatalf("Error: %v", err)
+		}
 	} else {
 		log.Fatalf("Error: wrong number of arguments")
 	}
